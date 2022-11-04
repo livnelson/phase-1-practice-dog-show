80661d55a31e2079bbdf6a6ed430864cb0d83fd3
Liv Nelson <livnelson@me.com>
2022-11-04 14:17:31 -0700

    completed lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,60 @@
-document.addEventListener('DOMContentLoaded', () => {
-
+fetch('http://localhost:3000/dogs')
+.then(response => response.json()) // DON"T FORGET THE PARENTHESES HERE!!
+.then(data => {
+    renderDogs(data)
 })
+
+function renderDogs(dogs) {
+    dogs.forEach(renderDog)
+    console.log(dogs)
+}
+
+// builder the dog data list
+function renderDog(dog) {
+    const dogForm = document.getElementById('table-body')
+    
+    const dogRow = document.createElement('tr')
+
+    const dogName = document.createElement('td')
+    const dogBreed = document.createElement('td')
+    const dogSex = document.createElement('td')
+    const editButton = document.createElement('button')
+
+    dogName.textContent = dog.name
+    dogBreed.textContent = dog.breed
+    dogSex.textContent = dog.sex
+    editButton.textContent = 'Edit Dog'
+    
+    dogRow.append(dogName, dogBreed, dogSex, editButton)
+    dogForm.append(dogRow)
+
+    editButton.addEventListener('click', (e) => {
+        // e.preventDefault()
+
+        const children = FORM.children;
+        const nameInput = children[0]
+        const breedInput = children[1]
+        const sexInput = children[2]
+
+        children[3].id.dogid
+
+
+       nameInput.value = dog.name;
+       breedInput.value = dog.sex;
+       sexInput.value = dog.breed;
+    
+        console.log('Ive been clicked')
+    })
+    // console.log(renderDog)
+}
+
+
+
+
+
+const init = () => {
+   
+
+}
+
+init()
